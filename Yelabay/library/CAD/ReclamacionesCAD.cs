9747b81d552bf78374778f8d9b2ff918b2c5c7ae
Andres Tebar Moreno <atm64@alu.ua.es>
2019-04-14 17:11:10 +0200

    Declarados modulos en ReclamacionesCAD

--- Yelabay/library/CAD/ReclamacionesCAD.cs
+++ Yelabay/library/CAD/ReclamacionesCAD.cs
@@ -8,5 +8,40 @@ namespace library
 {
     class ReclamacionesCAD
     {
+        class ReclamacionesCAD
+    {
+        public ReclamacionesCAD()
+        {
+            //se establece la conexion con la BD
+        }
+
+        public bool createReclamacion(ReclamacionesEN reclamacion)
+        {
+            bool creado = false;
+
+            return creado;
+        }
+
+        public bool updateReclamacion(ReclamacionesEN reclamacion)
+        {
+            bool updated = false;
+
+            return updated;
+        }
+
+        public bool readReclamacion(ReclamacionesEN reclamacion)
+        {
+            bool leida = false;
+
+            return leida;
+        }
+
+        public bool deleteReclamacion(ReclamacionesEN reclamacion)
+        {
+            bool borrada = false;
+
+            return borrada;
+        }
+
     }
 }
