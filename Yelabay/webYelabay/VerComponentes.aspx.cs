eb46b69b8a3151ce7914544555032eb85f234d6a
Wilson Monta Vargas <wmv1@alu.ua.es>
2019-05-23 23:23:11 +0200

    Agregados visores de categorias

---
+++ Yelabay/webYelabay/VerComponentes.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace webYelabay
+{
+    public partial class VerComponentes : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ImagenProduc_Command(object sender, CommandEventArgs e)
+        {
+            Response.Redirect("VerProducto.aspx?id=" + e.CommandArgument.ToString());
+        }
+    }
+}
