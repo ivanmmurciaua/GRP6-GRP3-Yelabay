a76c7e3419bf24441e1099ea1b0ebd3d3bbaf8af
Wilson Monta Vargas <wmv1@alu.ua.es>
2019-05-26 16:09:38 +0200

    Actualizacion EN y CAD

--- Yelabay/library/CAD/DevolucionesCAD.cs
+++ Yelabay/library/CAD/DevolucionesCAD.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,24 +10,149 @@ namespace library
 {
     class DevolucionesCAD
     {
+        private string constring;
         public DevolucionesCAD()
         {
-
+            constring = ConfigurationManager.ConnectionStrings["miconexion"].ToString();
         }
 
         public bool createDevolucion(DevolucionesEN devEn)
         {
-            return true;
+            bool exito = false;
+            SqlConnection c = new SqlConnection(constring);
+            try
+            {
+
+                c.Open();
+                SqlCommand com = new SqlCommand("insert into Devoluciones (motivo, fechadevolucion, fkpedido) VALUES (@motivo, @fechadevolucion, @fkpedido)", c);
+
+
+                com.Parameters.AddWithValue("@motivo", devEn.getMotivo().ToString());
+                com.Parameters.AddWithValue("@fechadevolucion", devEn.getFechaDevolucion().ToString());
+                com.Parameters.AddWithValue("@fkpedido", devEn.getCodigoPedido().ToString());
+
+                com.ExecuteNonQuery();
+
+                c.Close();
+                exito = true;
+
+
+            }
+            catch (Exception ex)
+            {
+                exito = false;
+                Console.WriteLine("User operation has failed.Error: { 0} ", ex.Message);
+                c.Close();
+
+            }
+            return exito;
         }
 
         public bool updateDevolucion(DevolucionesEN devEn)
         {
-            return true;
+            bool exito = false;
+
+            SqlConnection c = new SqlConnection(constring);
+            try
+            {
+                c.Open();
+                SqlCommand com = new SqlCommand("UPDATE Devoluciones SET motivo=@motivo, fechadevolucion=@fechadevolucion, fkpedido=@fkpedido where id=@id", c);
+
+                com.Parameters.AddWithValue("@motivo", devEn.getMotivo().ToString());
+                com.Parameters.AddWithValue("@fechadevolucion", devEn.getFechaDevolucion().ToString());
+                com.Parameters.AddWithValue("@fkpedido", devEn.getCodigoPedido().ToString());
+                com.Parameters.AddWithValue("@id", devEn.getId().ToString());
+
+
+                com.ExecuteNonQuery();
+
+                c.Close();
+                exito = true;
+
+
+            }
+            catch (Exception ex)
+            {
+                exito = false;
+                Console.WriteLine("User operation has failed.Error: { 0} ", ex.Message);
+                c.Close();
+
+            }
+
+
+            return exito;
         }
 
         public bool deleteDevolucion(DevolucionesEN devEn)
         {
-            return true;
+            bool exito = false;
+
+            SqlConnection c = new SqlConnection(constring);
+            try
+            {
+                c.Open();
+                SqlCommand com = new SqlCommand("DELETE FROM Devoluciones WHERE id = " + devEn.getId().ToString(), c);
+
+                com.ExecuteNonQuery();
+
+                c.Close();
+                exito = true;
+
+            }
+            catch (Exception ex)
+            {
+                exito = false;
+                Console.WriteLine("User operation has failed.Error: { 0} ", ex.Message);
+                c.Close();
+
+            }
+
+
+            return exito;
         }
+
+        public bool readDevolucion(DevolucionesEN devEn)
+        {
+            bool exito = false;
+
+            SqlConnection c = new SqlConnection(constring);
+            try
+            {
+                c.Open();
+                SqlCommand com = new SqlCommand("Select * from Devoluciones where id=@id", c);
+
+                com.Parameters.AddWithValue("@id", devEn.getId().ToString());
+
+                com.ExecuteNonQuery();
+                SqlDataReader dr = com.ExecuteReader();
+
+                if (dr.Read() == true)
+                {
+                    devEn.setMotivo(dr["motivo"].ToString());
+                    devEn.setFechaDevolucion(dr["fechadevolucion"].ToString());
+                    devEn.setCodigoPedido(Int32.Parse(dr["fkpedido"].ToString()));
+
+                    exito = true;
+
+
+                }
+                c.Close();
+
+
+
+            }
+            catch (Exception ex)
+            {
+                exito = false;
+                Console.WriteLine("User operation has failed.Error: { 0} ", ex.Message);
+                c.Close();
+
+            }
+
+
+            return exito;
+        }
+
+
     }
 }
